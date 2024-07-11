0bb934890eb7826054b26755094d2d4e855fc77f
Radek <radoslaw.grzymala@hotmail.com>
2024-07-11 11:43:38 +0200

    feat: separate service for emails was created

---
+++ src/app/services/emails/emails.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {API_CONFIG} from '../services.interface';
+import {catchError} from 'rxjs/operators';
+import {HandleErrorService} from '../handle-error/handle-error.service';
+
+export interface SendEmail {
+  message: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailsService {
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly handleErrorService: HandleErrorService
+  ) {
+  }
+
+  sendMail(email: string, base64: string): Observable<SendEmail> {
+    return this.httpClient.post<SendEmail>(API_CONFIG.EMAILS, {
+      email: email,
+      ticketBase64: base64
+    })
+      .pipe(
+        catchError((response) => this.handleErrorService.get(response))
+      );
+  }
+}
