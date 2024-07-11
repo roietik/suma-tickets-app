0ec2213bb833ee19deac4de7975421a6e02f2aa3
Radek <radoslaw.grzymala@hotmail.com>
2024-07-11 11:47:02 +0200

    style: removed unnecessary component from module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgForOf, NgOptimizedImage} from '@angular/common';
+import {NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton, MatFabButton} from '@angular/material/button';
@@ -19,7 +19,6 @@ import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component'
 import { MatDialogModule } from '@angular/material/dialog';
 import { UserFormComponent } from './user-form/user-form.component';
 import {UserListComponent} from './user-list/user-list.component';
-import {TicketComponent} from './ticket/ticket.component';
 import {MatCheckbox} from '@angular/material/checkbox';
 import {AdminComponent} from './admin/admin.component';
 
@@ -29,7 +28,6 @@ import {AdminComponent} from './admin/admin.component';
     ConfirmDialogComponent,
     UserFormComponent,
     UserListComponent,
-    TicketComponent,
     AdminComponent
   ],
   imports: [
@@ -58,13 +56,13 @@ import {AdminComponent} from './admin/admin.component';
     MatCheckbox,
     MatCardHeader,
     MatCardTitle,
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgIf
   ],
   exports: [
     ConfirmDialogComponent,
     UserFormComponent,
     UserListComponent,
-    TicketComponent,
     AdminComponent
   ]
 })
