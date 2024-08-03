92a42cfcc4d852b16657b0eddacf4b9f0e0aa684
Radek <radoslaw.grzymala@hotmail.com>
2024-08-03 04:56:39 +0200

    test: new test cases for users service

--- src/app/services/users/users.service.spec.ts
+++ src/app/services/users/users.service.spec.ts
@@ -1,16 +1,111 @@
 import { TestBed } from '@angular/core/testing';
+import {User, UsersService} from './users.service';
+import {HttpClientTestingModule, HttpTestingController, TestRequest} from '@angular/common/http/testing';
+import {Observable} from 'rxjs';
+import {API_CONFIG, BaseGiven} from '../services.interface';
 
-import { UsersService } from './users.service';
+interface When {
+  getAll: () => Observable<User[]>;
+  create: () => Observable<User>;
+  isEmailExist: (email: string) => Observable<boolean>;
+}
 
-describe('UsersService', () => {
-  let service: UsersService;
+interface Then {
+  expectUrl: (expectedUrl: string, type: string) => TestRequest;
+}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UsersService);
+type Given = BaseGiven<When, Then>;
+
+const IsEmailExistResponse = true;
+
+const userResponse: User = {
+  id: 1,
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@gmail.com',
+  ticketBase64: 'base64'
+};
+
+const usersResponse: User[] = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe@gmail.com',
+    ticketBase64: 'base64'
+  },
+  {
+    id: 2,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe2@gmail.com',
+    ticketBase64: 'base64'
+  }
+];
+
+describe('UsersService', (): void => {
+  const given = async (response: User[] | User | boolean): Promise<Given> => {
+    await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ UsersService ]
+    });
+
+    return {
+      when: {
+        getAll: (): Observable<User[]> => TestBed.inject(UsersService).getAll(),
+        create: (): Observable<User> => TestBed.inject(UsersService).create({
+            id: 1,
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'johndoe@gmail.com',
+            ticketBase64: 'base64'
+          }),
+        isEmailExist: (email: string): Observable<boolean> => TestBed.inject(UsersService).isEmailExist(email),
+      },
+      then: {
+        expectUrl: (expectedUrl: string, type: string): TestRequest => {
+          const httpTestingController = TestBed.inject(HttpTestingController);
+          const req = httpTestingController.expectOne(expectedUrl);
+          expect(req.request.method).toEqual(type);
+          req.flush(response);
+          httpTestingController.verify();
+
+          return req;
+        }
+      }
+    };
+  };
+
+  it('should return users list', async (): Promise<void> => {
+    const {when, then} = await given(usersResponse);
+
+    when.getAll().subscribe((users): void => {
+      expect(users).toEqual(usersResponse);
+    });
+
+    const req = then.expectUrl(API_CONFIG.USERS, 'GET');
+    expect(req.request.method).toEqual('GET');
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('after created should return user', async (): Promise<void> => {
+    const {when, then} = await given(userResponse);
+
+    when.create().subscribe((user): void => {
+      expect(user).toEqual(userResponse);
+    });
+
+    const req = then.expectUrl(API_CONFIG.USERS, 'POST');
+    expect(req.request.method).toEqual('POST');
+  });
+
+  it('is email exist', async (): Promise<void> => {
+    const {when, then} = await given(IsEmailExistResponse);
+
+    when.isEmailExist('johndoe@gmail.com').subscribe((isEmailExist): void => {
+      expect(isEmailExist).toEqual(true);
+    });
+
+    const req = then.expectUrl(`${API_CONFIG.USERS}/is-email-exist`, 'POST');
+    expect(req.request.method).toEqual('POST');
   });
 });
